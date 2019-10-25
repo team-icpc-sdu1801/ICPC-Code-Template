7101b98f7bcebf30020cd778fec103f959ce6a69
zhao408639122 <408639122@qq.com>
2019-10-25 20:48:54 +0800

    AC自动机notation

--- 字符串/AC自动机.cpp
+++ 字符串/AC自动机.cpp
@@ -70,3 +70,61 @@ int main()
     }
     return 0;
 }
+//有很多题需要判断是否是终点，在求fail结点的时候可以顺便求一下
+if (tr[u][i]) fail[tr[u][i]] = tr[fail[u]][i], idx[tr[u][i]] |= idx[fail[tr[u][i]]], q.push(tr[u][i]);
+//矩阵快速幂求出现过给定字符串的的所有长度的串
+typedef unsigned long long ull;
+struct rec{
+	int sz;
+	ull a[N][N];
+	rec(){}
+	void init(int _sz){
+		sz = _sz;
+		memset(a, 0, sizeof(a));
+	}
+	rec operator* (const rec &b) const {
+		rec res;
+		res.init(sz);
+		for (int i = 0; i <= sz; ++i){
+			for (int j = 0; j <= sz; ++j){
+				for (int k = 0; k <= sz; ++k)
+					res.a[i][j] += a[i][k] * b.a[k][j];
+			}
+		}
+		return res;
+	}
+}e, ans, res;
+inline rec qpow(rec a, long long q){
+	rec res = e;
+	for (;q; q >>= 1, a = a * a){
+		if (q & 1) res = res * a;
+	}
+	return res;
+}
+//省略了init和insert
+namespace AC{
+	queue<int>q;
+	void build(){
+		for (int i = 0; i < 26; ++i)
+			if (tr[0][i]) q.push(tr[0][i]);
+		while(!q.empty()){
+			int u = q.front();
+			q.pop();
+			for (int i = 0; i < 26; ++i){
+				if (tr[u][i]) fail[tr[u][i]] = tr[fail[u]][i], idx[tr[u][i]] |= idx[fail[tr[u][i]]], q.push(tr[u][i]);
+				else tr[u][i] = tr[fail[u]][i];
+			}
+		}
+	}
+	inline rec chengzi(){
+		res.init(tot + 1);
+		for (int i = 0; i <= tot; ++i){
+			if (idx[i]) res.a[i][tot + 1] = 1, res.a[i][i] = 26; 
+			else for (int j = 0; j < 26; ++j){
+				res.a[i][tr[i][j]]++;
+			}
+		}
+		res.a[tot + 1][tot + 1] = 1;
+		return res;
+	}
+};
