598a203466b6cb6ef99af2f0d91319317a20388e
zhao408639122 <408639122@qq.com>
2019-10-27 15:59:01 +0800

    补充kmp

--- 字符串/kmp.cpp
+++ 字符串/kmp.cpp
@@ -7,6 +7,16 @@ using namespace std;
 const int N = 10010;
 char s[N];
 int nxt[N];
+//求 t 在 s 中是否出现，需要t的nxt数组 
+inline int check(char *s, int sl, char *t, int tl, int *nxt){
+	int l = 0;
+	for (int i = 1; i <= sl; ++i){
+		while(l && s[i] != t[l + 1]) l = nxt[l];
+		if (s[i] == t[l + 1]) l++;
+		if (l == tl) return 1;
+	}
+	return 0;
+}
 int main(){
     scanf("%s", s + 1);
     int p = 0, n = strlen(s + 1);
